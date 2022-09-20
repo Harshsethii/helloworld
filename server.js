4688fb2fdd57c8e0366d7113a2bb3e80e51435c6
Harsh sethi <hsethi5@myseneca.ca>
2022-09-19 22:53:25 -0400

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Harsh Sethi - 121889216!");
 });
 
 // setup http server to listen on HTTP_PORT
